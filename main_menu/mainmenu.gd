021f1d1c7be34620fed0eafff0793d95829cf409
Zapzoop <turtlemaster700s@gmail.com>
2023-12-28 13:10:29 +0530

    Glowing Buttons

--- main_menu/mainmenu.gd
+++ main_menu/mainmenu.gd
@@ -33,3 +33,28 @@ func loads():
 func save(content):
 	var file = FileAccess.open("hight_Score.txt.txt", FileAccess.WRITE)
 	file.store_string(str(content))
+
+
+func _on_start_mouse_entered():
+	$VBoxContainer/start.modulate = Color(2,1,1,1)
+
+func _on_option_mouse_entered():
+	$VBoxContainer/option.modulate = Color(1,2,1,1)
+
+func _on_credits_mouse_entered():
+	$VBoxContainer/credits.modulate = Color(1,2,1,1)
+
+func _on_exit_mouse_entered():
+	$VBoxContainer/exit.modulate = Color(1,2,1,1)
+
+func _on_exit_mouse_exited():
+	$VBoxContainer/exit.modulate = Color(1,1,1,1)
+
+func _on_option_mouse_exited():
+	$VBoxContainer/option.modulate = Color(1,1,1,1)
+
+func _on_start_mouse_exited():
+	$VBoxContainer/start.modulate = Color(1,1,1,1)
+
+func _on_credits_mouse_exited():
+	$VBoxContainer/credits.modulate = Color(1,1,1,1)
